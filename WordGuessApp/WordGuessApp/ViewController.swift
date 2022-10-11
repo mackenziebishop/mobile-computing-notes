42f880370914aa187a008ecfa4b3411237dbdb76
Mackenzie <mackenzieb359@gmail.com>
2022-10-11 08:55:41 -0500

    Added comments and onclick functions

--- WordGuessApp/WordGuessApp/ViewController.swift
+++ WordGuessApp/WordGuessApp/ViewController.swift
@@ -29,7 +29,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Check button should be disabled before guess
+        //Get the first word from the array
+        //Populate guessEntered with underscores. # of underscores is equal to the # of characters in the guess
+        //Get first hint associated with the word being guessed in the array
+        //Clear status label initially
+    }
+    
+    @IBAction func checkButtonClicked(_ sender: Any){
+        //Get text from text field
+        //Replace the guessed letter if the letter is part of the word
+        //Assigning the word to guessStatus after a guess
+        //If the word is guessed correctly, enable play again button and disabling the check button
+    }
+    
+    @IBAction func playAgainButtonClicked(_ sender: UIButton){
+        //Reset button to disable initially
+        //Clear the label
+        //If count reaches end of array, print "Congratulations" in hintLabel (if-else statement)
+        //If...
+            //Clear labels
+        //Else...
+            //Fetch next word from array
+            //Fetch hint correlated to word
+            //Enabling the check button
+        
     }
 
 
