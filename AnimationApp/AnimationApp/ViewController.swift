4643d2a0501473cce9bc45a539286b98fad1ea1b
Mackenzie <mackenzieb359@gmail.com>
2022-10-18 08:29:01 -0500

    Added constraints, connections and actions

--- AnimationApp/AnimationApp/ViewController.swift
+++ AnimationApp/AnimationApp/ViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
+    @IBOutlet weak var happyOutlet: UIButton!
+    @IBOutlet weak var sadOutlet: UIButton!
+    @IBOutlet weak var angryOutlet: UIButton!
+    @IBOutlet weak var shakeOutlet: UIButton!
+    @IBOutlet weak var showMeOutlet: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func happyButtonClicked(_ sender: Any) {
+    }
+    @IBAction func sadButtonClicked(_ sender: Any) {
+    }
+    @IBAction func angryButtonClicked(_ sender: Any) {
+    }
+    @IBAction func shakeButtonClicked(_ sender: Any) {
+    }
+    @IBAction func showButtonClicked(_ sender: Any) {
+    }
+    
 
 
 }
