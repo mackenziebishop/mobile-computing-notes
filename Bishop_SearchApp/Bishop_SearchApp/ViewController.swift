45c40569a209ebf110727ef0468244d2e07c3fff
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-19 20:21:25 -0500

    HW 4 Search App added

---
+++ Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bishop_SearchApp
+//
+//  Created by Bishop,Mackenzie N on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
