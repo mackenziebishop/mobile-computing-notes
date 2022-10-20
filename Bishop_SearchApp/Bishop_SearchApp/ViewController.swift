1ed7fbfa5baf9025ca903f50f27005793c01e686
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-19 20:58:21 -0500

    Added constraints and UI

--- Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
+++ Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButtonAction: UIButton!
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var showPrevImagesBtn: UIButton!
+    @IBOutlet weak var showNextImagesBtn: UIButton!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
