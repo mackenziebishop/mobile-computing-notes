151eeee033b55a8a8f99ef03159d15aa2a6e17c1
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-19 21:06:20 -0500

    Added image array

--- Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
+++ Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
@@ -18,6 +18,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var topicInfoText: UITextView!
     
+    let image_array = [
+            //Flower image array
+            [UIImage(named: "daisy"), UIImage(named: "lilly"), UIImage(named: "marigold"), UIImage(named: "rose"), UIImage(named: "sunflower")],
+            //Book image array
+            [UIImage(named: "dune"), UIImage(named: "great_gatsby"), UIImage(named: "little_women"), UIImage(named: "pride_and_prejudice"), UIImage(named: "wuthering_heights")],
+            //Actor image array
+            [UIImage(named: "emma_watson"), UIImage(named: "meryl_streep"), UIImage(named: "morgan_freeman"), UIImage(named: "tom_hanks"), UIImage(named: "tom_holland")]
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
