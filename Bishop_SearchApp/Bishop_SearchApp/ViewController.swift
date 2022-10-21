27dddd5b017834bbd1e6c0b6c85614e80fd5ea31
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-20 21:50:26 -0500

    Successful SearchApp build

--- Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
+++ Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
@@ -19,12 +19,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var topicInfoText: UITextView!
     
     let image_array = [
-            //Flower image array
-            [UIImage(named: "daisy"), UIImage(named: "lilly"), UIImage(named: "marigold"), UIImage(named: "rose"), UIImage(named: "sunflower")],
-            //Book image array
-            [UIImage(named: "dune"), UIImage(named: "great_gatsby"), UIImage(named: "little_women"), UIImage(named: "pride_and_prejudice"), UIImage(named: "wuthering_heights")],
-            //Actor image array
-            [UIImage(named: "emma_watson"), UIImage(named: "meryl_streep"), UIImage(named: "morgan_freeman"), UIImage(named: "tom_hanks"), UIImage(named: "tom_holland")]
+    ["daisy", "lilly", "marigold", "rose", "sunflower", "These are some of my favorite flowers that can be found locally. This search yields 5 results."],
+    
+    ["dune", "great_gatsby", "little_women", "pride_and_prejudice", "wuthering_heights", "These are some of my favorite books that range from classical literature to modern science fiction. This search yields 5 results."],
+    
+    ["emma_watson", "meryl_streep", "morgan_freeman", "tom_hanks", "tom_holland", "These are some of the most well-known actors and actresses of all time, spanning across different generations of movie and TV history. This search yields 5 results."],
     ]
     //Flower keyword array
     let flower_keywords = ["flower", "color", "size", "petals"]
@@ -35,20 +34,70 @@ class ViewController: UIViewController {
     //Actor keyword array
     let actor_keywords = ["actor", "movie", "hero", "film"]
     
-    //Topic array
-        let topic_array = [["These are some of my favorite flowers that can be found locally. This search yields 5 results."],["These are some of my favorite books that range from classical literature to modern science fiction. This search yields 5 results."],["These are some of the most well-known actors and actresses of all time, spanning across different generations of movie and TV history. This search yields 5 results."]]
-    
+    var imageNum = 0;
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    @IBAction func onSearchBtnClicked(_ sender: Any) {
-    }
-    @IBAction func onNextBtnClicked(_ sender: Any) {
-    }
-    @IBAction func onPrevBtnClicked(_ sender: Any) {
+        topicDetails(0)
+        showPrevImagesBtn.isEnabled = false
+        showNextImagesBtn.isEnabled = false
     }
     
+    @IBAction func onSearchBtnClicked(_ sender: UIButton) {
+        var topic = 0
+        
+        if flower_keywords.contains(searchTextField.text!){
+            topic = 1
+        }
+        if book_keywords.contains(searchTextField.text!){
+            topic = 2
+        }
+        if actor_keywords.contains(searchTextField.text!){
+            topic = 3
+        }
+        
+        switch (topic){
+        case 1:
+            showNextImagesBtn.isEnabled = true
+            for image in image_array{
+                resultImage.image = UIImage(named: image_array[0][1])
+            }
 
+        case 2:
+            showNextImagesBtn.isEnabled = true
+            
+        case 3:
+            showNextImagesBtn.isEnabled = true
+            
+        default:
+            topicInfoText.text = "Keyword yields no results."
+        }
+    }
+    
+    @IBAction func onNextBtnClicked(_ sender: Any) {
+        imageNum += 1;
+        topicDetails(imageNum)
+        showPrevImagesBtn.isEnabled = true;
+        
+        if (imageNum == image_array.count-1){
+            showNextImagesBtn.isEnabled = false
+        }
+    }
+    
+    @IBAction func onPrevBtnClicked(_ sender: Any) {
+        imageNum -= 1;
+        topicDetails(imageNum)
+        showNextImagesBtn.isEnabled = true;
+        
+        if (imageNum == 0){
+            showPrevImagesBtn.isEnabled = false;
+        }
+    }
+    
+    func topicDetails(_ imageNum:Int){
+        resultImage.image = UIImage(named: image_array[imageNum][0])
+        topicInfoText.text = image_array[imageNum][5]
+    }
 }
 
