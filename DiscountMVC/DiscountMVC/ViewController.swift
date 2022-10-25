d07638d785aa6552e8913fa1c8cb3ada6b22affb
Mackenzie <mackenzieb359@gmail.com>
2022-10-25 08:37:29 -0500

    Added multiple views and segue

--- DiscountMVC/DiscountMVC/ViewController.swift
+++ DiscountMVC/DiscountMVC/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountField: UITextField!
+    @IBOutlet weak var discountField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcButtonClicked(_ sender: Any) {
+    }
+    
 }
 
---
+++ DiscountMVC/DiscountMVC/resultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  resultViewController.swift
+//  DiscountMVC
+//
+//  Created by Bishop,Mackenzie N on 10/25/22.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    @IBOutlet weak var displayAmountOutlet: UILabel!
+    
+    @IBOutlet weak var discountRateOutlet: UILabel!
+    
+    @IBOutlet weak var displayResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
