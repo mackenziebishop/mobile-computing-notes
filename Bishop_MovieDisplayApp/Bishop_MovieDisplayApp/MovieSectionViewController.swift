e2febd0f411ac40a8a9bd7e1befa0869b8b96e5f
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-11-29 10:20:18 -0600

    Embed and added files

---
+++ Bishop_MovieDisplayApp/Bishop_MovieDisplayApp/MovieSectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bishop_MovieDisplayApp
+//
+//  Created by Bishop,Mackenzie N on 11/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
